effcb961ed92f6d85cff046922896f3acfffaec1
Atle Frenvik Sveen <atle.sveen@gmail.com>
2021-07-28 09:22:29 +0200

    test v2, again

--- script.ps1
+++ script.ps1
@@ -2,6 +2,7 @@ $GhPAT = "${env:GITHUBPAT}"
 
 echo $GhPAT
 
+git remote remove gh
 git remote add gh "https://atlefren:$GhPAT@github.com/atlefren/mytest.git"
 git push -u gh master
 git remote remove gh
