610cb3eee2059925656f8e1d7ee64cbac7a4dbf4
Atle Frenvik Sveen <atle.sveen@gmail.com>
2021-07-28 09:29:16 +0200

    meh

--- script.ps1
+++ script.ps1
@@ -3,6 +3,9 @@ $GhPAT = "${env:GITHUBPAT}"
 echo "PAT:"
 echo $GhPAT
 
+echo "REFS:"
+git show-ref
+
 echo "add remote"
 git remote add gh "https://atlefren:$GhPAT@github.com/atlefren/mytest.git"
 echo "push"
