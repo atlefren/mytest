3dddd2e30edd0f50a3a148d30f69918e76ae992e
Atle Frenvik Sveen <atle.sveen@gmail.com>
2021-07-28 13:59:06 +0200

    fix typos

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$GhPAT = "${env:GITHUBPAT}"
+$GhPAT = "${env:GITHUBPATH}"
 git remote add gh "https://atlefren:$GhPAT@github.com/atlefren/mytest.git"
 git push -u gh HEAD:master
 git remote remove gh
